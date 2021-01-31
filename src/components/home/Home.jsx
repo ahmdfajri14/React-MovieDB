1969048d6a34e55f88cbf3cb712cfcc8bfe40f40
Fajri <achmad.fajriansyah@qoala.co.id>
2021-01-31 23:07:13 +0700

    react movie

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,8 +3,6 @@ import {
   fetchMovies,
   fetchGenre,
   fetchMovieByGenre,
-  fetchPersons,
-  fetchTopratedMovie,
 } from "../../service";
 import RBCarousel from "react-bootstrap-carousel";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
@@ -15,24 +13,40 @@ export function Home() {
   const [nowPlaying, setNowPlaying] = useState([]);
   const [genres, setGenres] = useState([]);
   const [movieByGenre, setMovieByGenre] = useState([]);
-  const [persons, setPersons] = useState([]);
-  const [topRated, setTopRated] = useState([]);
+	// const [favourites, setFavourites] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       setNowPlaying(await fetchMovies());
       setGenres(await fetchGenre());
       setMovieByGenre(await fetchMovieByGenre(28));
-      setPersons(await fetchPersons());
-      setTopRated(await fetchTopratedMovie());
     };
-
     fetchAPI();
   }, []);
 
   const handleGenreClick = async (genre_id) => {
     setMovieByGenre(await fetchMovieByGenre(genre_id));
   };
+
+  // const saveToLocalStorage = (items) => {
+	// 	localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	// };
+
+  // const addFavouriteMovie = (movie) => {
+  //   console.log(movie, "movie")
+  //   const newFavouriteList = [...newFavouriteList, movie];
+  //   setFavourites(newFavouriteList);
+	// 	saveToLocalStorage(newFavouriteList);
+  // }
+
+  // const removeFavouriteMovie = (movie) => {
+	// 	const newFavouriteList = favourites.filter(
+	// 		(favourite) => favourite.imdbID !== movie.imdbID
+	// 	);
+
+	// 	setFavourites(newFavouriteList);
+	// 	saveToLocalStorage(newFavouriteList);
+	// };
   const movies = nowPlaying.slice(0, 5).map((item, index) => {
     return (
       <div style={{ height: 500, width: "100%" }} key={index}>
@@ -57,21 +71,14 @@ export function Home() {
 
   const genreList = genres.map((item, index) => {
     return (
-      <li className="list-inline-item" key={index}>
-        <button
-          type="button"
-          className="btn btn-outline-info"
-          onClick={() => {
-            handleGenreClick(item.id);
-          }}
-        >
-          {item.name}
-        </button>
-      </li>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown" key={index}>
+         <a className="dropdown-item" onClick={handleGenreClick()}>{item.name}</a>
+         </div>
     );
   });
 
   const movieList = movieByGenre.slice(0, 4).map((item, index) => {
+   console.log(item, "cek item")
     return (
       <div className="col-md-3 col-sm-6" key={index}>
         <div className="card">
@@ -80,46 +87,20 @@ export function Home() {
           </Link>
         </div>
         <div className="mt-3">
-          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
-          <p>Rated: {item.rating}</p>
-          <ReactStars
-            count={item.rating}
-            size={20}
-            color1={"#f4c10f"}
-          ></ReactStars>
-        </div>
-      </div>
-    );
-  });
-
-  const trendingPersons = persons.slice(0, 4).map((p, i) => {
-    return (
-      <div className="col-md-3 text-center" key={i}>
-        <img
-          className="img-fluid rounded-circle mx-auto d-block"
-          src={p.profileImg}
-          alt={p.name}
-        ></img>
-        <p className="font-weight-bold text-center">{p.name}</p>
-        <p
-          className="font-weight-light text-center"
-          style={{ color: "#5a606b" }}
-        >
-          Trending for {p.known}
-        </p>
-      </div>
-    );
-  });
-
-  const topRatedList = topRated.slice(0, 4).map((item, index) => {
-    return (
-      <div className="col-md-3" key={index}>
-        <div className="card">
-          <Link to={`/movie/${item.id}`}>
-            <img className="img-fluid" src={item.poster} alt={item.title}></img>
-          </Link>
-        </div>
-        <div className="mt-3">
+        <p className='mr-2' >Add to Favourites</p>
+			<svg
+				width='1em'
+				height='1em'
+				viewBox='0 0 16 16'
+				className='bi bi-heart-fill'
+				fill='red'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path
+					fill-rule='evenodd'
+					d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z'
+				/>
+			</svg>
           <p style={{ fontWeight: "bolder" }}>{item.title}</p>
           <p>Rated: {item.rating}</p>
           <ReactStars
@@ -134,6 +115,28 @@ export function Home() {
 
   return (
     <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Cinema 22</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Your Favorit</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Search by Genre
+        </a>
+        {genreList}
+      </li>
+    </ul>
+  </div>
+</nav>
       <div className="row mt-2">
         <div className="col">
           <RBCarousel
@@ -147,13 +150,6 @@ export function Home() {
           </RBCarousel>
         </div>
       </div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <ul className="list-inline">{genreList}</ul>
-        </div>
-      </div>
-
       <div className="row mt-3">
         <div className="col">
           <div className="float-right">
@@ -162,111 +158,6 @@ export function Home() {
         </div>
       </div>
       <div className="row mt-3">{movieList}</div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <p className="font-weight-bold" style={{ color: "#5a606b" }}>
-            TRENDING PERSONS ON THIS WEEK
-          </p>
-        </div>
-      </div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <div className="float-right">
-            <i className="far fa-arrow-alt-circle-right"></i>
-          </div>
-        </div>
-      </div>
-      <div className="row mt-3">{trendingPersons}</div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <p className="font-weight-bold" style={{ color: "#5a606b" }}>
-            TOP RATED MOVIES
-          </p>
-        </div>
-      </div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <div className="float-right">
-            <i className="far fa-arrow-alt-circle-right"></i>
-          </div>
-        </div>
-      </div>
-      <div className="row mt-3">{topRatedList}</div>
-
-      <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
-
-      <div className="row mt-3 mb-5">
-        <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>ABOUT ME</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-            error earum perspiciatis praesentium sint ipsum provident blanditiis
-            pariatur necessitatibus voluptas, cum, atque iste eligendi autem,
-            culpa cupiditate placeat facilis repellat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
-            dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
-            earum?
-          </p>
-          <ul className="list-inline">
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-youtube"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-4 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>KEEP IN TOUCH</h3>
-          <ul className="list-unstyled">
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Address:
-                </strong>{" "}
-                city, state, country
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Phone:
-                </strong>{" "}
-                +01 00 00 00
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-envelope"></i> Email:
-                </strong>{" "}
-                info@infomail.com
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
--- src/service/index.js
+++ src/service/index.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 const apiKey = 'a4999a28333d1147dbac0d104526337a';
 const url = 'https://api.themoviedb.org/3';
 const nowPlayingUrl = `${url}/movie/now_playing`;
-const topratedUrl = `${url}/movie/top_rated`;
 const movieUrl = `${url}/movie`;
 const genreUrl = `${url}/genre/movie/list`;
 const moviesUrl = `${url}/discover/movie`;
-const personUrl = `${url}/trending/person/week`;
 
 export const fetchMovies = async () => {
     try {
@@ -76,49 +74,6 @@ export const fetchMovieByGenre = async (genre_id) => {
     } catch (error) { }
 }
 
-export const fetchPersons = async () => {
-    try {
-        const { data } = await axios.get(personUrl, {
-            params: {
-                api_key: apiKey
-            }
-        })
-        const modifiedData = data['results'].map((p) => ({
-            id: p['id'],
-            popularity: p['popularity'],
-            name: p['name'],
-            profileImg: 'https://image.tmdb.org/t/p/w200' + p['profile_path'],
-            known: p['known_for_department']
-        }))
-        return modifiedData;
-    } catch (error) { }
-}
-
-export const fetchTopratedMovie = async () => {
-    try {
-        const { data } = await axios.get(topratedUrl, {
-            params: {
-                api_key: apiKey,
-                language: 'en_US',
-                page: 1
-            }
-        })
-        const posterUrl = 'https://image.tmdb.org/t/p/original/';
-        const modifiedData = data['results'].map((m) => ({
-            id: m['id'],
-            backPoster: posterUrl + m['backdrop_path'],
-            popularity: m['popularith'],
-            title: m['title'],
-            poster: posterUrl + m['poster_path'],
-            overview: m['overview'],
-            rating: m['vote_average'],
-        }))
-
-        return modifiedData;
-    } catch (error) {
-
-    }
-}
 
 export const fetchMovieDetail = async (id) => {
     try {
