390f21ddba1d8873dbbe9d838686da03ced6e57d
Fajri <achmad.fajriansyah@qoala.co.id>
2021-04-11 21:39:08 +0700

    Updates

---
+++ precache-manifest.a01ff10e39b8bffa49cab1f097419639.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c6b56756ad9d20eba42b23c08ad99c",
+    "url": "/React-MovieDB/index.html"
+  },
+  {
+    "revision": "9bdc25515a5f98408ef3",
+    "url": "/React-MovieDB/static/css/2.d1dbc715.chunk.css"
+  },
+  {
+    "revision": "92edfa6fb73b29612b39",
+    "url": "/React-MovieDB/static/css/main.3524d348.chunk.css"
+  },
+  {
+    "revision": "9bdc25515a5f98408ef3",
+    "url": "/React-MovieDB/static/js/2.d6e8d91e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-MovieDB/static/js/2.d6e8d91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92edfa6fb73b29612b39",
+    "url": "/React-MovieDB/static/js/main.840eb258.chunk.js"
+  },
+  {
+    "revision": "e3114d297dab5b264168",
+    "url": "/React-MovieDB/static/js/runtime-main.42e01992.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/React-MovieDB/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/React-MovieDB/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/React-MovieDB/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/React-MovieDB/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
